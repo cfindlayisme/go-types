207b35602ba8bd608498e8e7d9e44386ef65850a
Chuck Findlay <chuck@findlayis.me>
2024-12-08 00:58:43 -0400

    length type

---
+++ units/length.go
@@ -0,0 +1,21 @@
+package units
+
+type Length struct {
+	meters float64
+}
+
+func NewLengthFromMeters(m float64) Length {
+	return Length{meters: m}
+}
+
+func NewLengthFromFeet(f float64) Length {
+	return Length{meters: f * 0.3048}
+}
+
+func (l Length) ToMeters() float64 {
+	return l.meters
+}
+
+func (l Length) ToFeet() float64 {
+	return l.meters / 0.3048
+}
